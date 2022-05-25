810e27323cd4bc9c16716091cb726c2e9e7fbf63
Casper Wortmann <casper@MacBook-Pro-van-Casper-2.local>
2022-05-25 13:27:15 +0300

    Bash script.

--- code/contrastive_mm2/bash_script.py
+++ code/contrastive_mm2/bash_script.py
@@ -3,5 +3,6 @@
 import sys
 import subprocess
 
-for cmd in [['python', 'gru_test2.py', '--num_epochs 1'], ['python','gru_test2.py', '--num_epochs', '1', '--test_evalchange']]:
-    subprocess.run([sys.executable, cmd], shell=False)
+# for cmd in ['python', 'gru_test2.py', '--num_epochs 1'], ['python','gru_test2.py', '--num_epochs', '1', '--test_evalchange']]:
+cmd = ['python', 'gru_test2.py', '--num_epochs 1']
+subprocess.run([sys.executable, cmd], shell=False)
