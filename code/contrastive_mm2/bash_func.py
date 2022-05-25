341670e147b1cc6f94e581cfc4aa4a94b663df40
Casper Wortmann <casper@MacBook-Pro-van-Casper-2.local>
2022-05-25 13:20:13 +0300

    Bash script.

---
+++ code/contrastive_mm2/bash_func.py
@@ -0,0 +1,5 @@
+from time import sleep
+
+print("bash_func")
+sleep(3)
+print("bye!")
---
+++ code/contrastive_mm2/bash_script.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+
+import sys
+import subprocess
+
+for cmd in ['bash_func.py', 'bash_func.py', 'bash_func.py']:
+    subprocess.run([sys.executable, cmd], shell=False)
