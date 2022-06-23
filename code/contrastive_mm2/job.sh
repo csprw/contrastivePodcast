d93626c22c5186a603205f673ea52ac95d3107d1
Casper Wortmann <casper@mbpvancasper2.atlantis.local>
2022-06-23 09:34:10 +0200

    Job script added.

--- code/contrastive_mm2/job.sh
+++ code/contrastive_mm2/job.sh
@@ -5,11 +5,11 @@ python evaluate.py --calc_acc --model_weights_path logs/run4-clip_loss_None_lstm
 
 echo "script 7:"
 #python evaluate.py --calc_acc --model_weights_path logs/run3-clip_loss_None_sph_768_False_2022-06-14_08-53-55/output/full_model_epoch_5_weights.pt
-python evaluate.py --calc_acc --model_weights_path logs/run4-clip_loss_None_lstm_768_False_2022-06-17_19-59-06/output/full_model_epoch_0_weights.pt
+python evaluate.py --calc_acc --model_weights_path logs/run4-clip_loss_None_lstm_768_False_2022-06-17_19-59-06/output/full_model_epoch_4_weights.pt
 
 echo "Script 8:"
 #python evaluate.py --calc_acc --model_weights_path logs/run3-clip_loss_None_sph_768_False_2022-06-14_08-53-55/output/full_model_epoch_9_weights.pt
-#python evaluate.py --calc_acc --model_weights_path logs/run4-clip_loss_None_lstm_768_False_2022-06-17_19-59-06/output/full_model_epoch_0_weights.pt
+#python evaluate.py --calc_acc --model_weights_path logs/run4-clip_loss_None_lstm_768_False_2022-06-17_19-59-06/output/full_model_epoch_9_weights.pt
 echo "Bash script done."
 
 
